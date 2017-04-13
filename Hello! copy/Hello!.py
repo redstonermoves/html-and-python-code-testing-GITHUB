65c013079f1df18671c848054e0813fe4bf3e92f
redstonermoves <killianr1@gmail.com>
2017-04-13 16:38:32 -0400

    just added 'Hello!' and renamed to get rid of 'copy'
    yay!

---
+++ Hello! copy/Hello!.py
@@ -0,0 +1,10 @@
+print("Welcome!")
+name = input("hey whats your name? ")
+age = input("Now, " + name + " whats your age? ")
+if age == 11:
+    print("Oh! wow what are the chances you are the same age as me!")
+if name == "killian" or "Killian":
+    print("Oh! wow what are the chances you have the same name as me!")
+print("Thank you for using my program!")
+print("BY HTTP://WWW.REDSTONERMOVES.COM/")
+# If you use this DO NOT get rid of the last lines!!!!!! [line-9 and line-10]
